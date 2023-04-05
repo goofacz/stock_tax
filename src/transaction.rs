100a254dafea27baad1f5e11d2f565667880dff4
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2023-04-05 23:45:33 +0200

    Improve Currency and add CurrencyRates

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,4 +1,7 @@
+use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum Currency {
@@ -6,4 +9,16 @@ pub enum Currency {
     USD,
     GBP,
     EUR,
+}
+
+#[derive(Debug)]
+pub struct CurrencyRates {
+    pub currency: Currency,
+    pub rates: HashMap<NaiveDate, f32>,
+}
+
+impl fmt::Display for Currency {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
 }
