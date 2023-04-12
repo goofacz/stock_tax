9299213b752359445c718baef893951bd0dc7e3f
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2023-04-13 00:43:24 +0200

    Add Transaction
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,1 +1,21 @@
+use crate::currency::Currency;
+use chrono::NaiveDateTime;
 
+pub enum Operation {
+    Buy {
+        quantity: f64,
+        price: Box<dyn Currency>,
+        commision: Box<dyn Currency>,
+    },
+    Sell {
+        quantity: f64,
+        price: Box<dyn Currency>,
+        commision: Box<dyn Currency>,
+    },
+}
+
+pub struct Transaction {
+    pub symbol: String,
+    pub timestamp: NaiveDateTime,
+    pub operation: Operation,
+}
