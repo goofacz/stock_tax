88f0ddd1b13c1c69ec115338861c4a324faa6f55
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2023-04-16 01:03:13 +0200

    Refactor
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

---
+++ src/convert.rs
@@ -0,0 +1,74 @@
+use crate::interactive_brokers;
+use crate::mbank;
+use chrono::NaiveDateTime;
+use clap::{Args, ValueEnum};
+use derive_more::{Display, Error};
+use serde_json;
+use std::error;
+use std::fs::OpenOptions;
+use std::path::Path;
+
+#[derive(Display, Error, Debug)]
+pub struct Error {
+    reason: String,
+}
+
+#[derive(Args)]
+pub struct CommandArgs {
+    source: ConvertSource,
+    path: String,
+}
+
+#[derive(Display, Clone, ValueEnum)]
+enum ConvertSource {
+    Mbank,
+    InteractiveBrokers,
+}
+
+pub fn command(args: &CommandArgs) -> Result<(), Box<dyn error::Error>> {
+    let path = Path::new(&args.path);
+    let mut activities = match &args.source {
+        ConvertSource::Mbank => mbank::convert(&path).unwrap(),
+        ConvertSource::InteractiveBrokers => interactive_brokers::convert(&path).unwrap(),
+    };
+
+    activities.sort_by_key(|activity| activity.timestamp);
+
+    let begin_date = match activities.first() {
+        Some(date) => date,
+        _ => {
+            return Err(Box::new(Error {
+                reason: "No activities".to_string(),
+            }))
+        }
+    };
+    let end_date = match activities.last() {
+        Some(date) => date,
+        _ => {
+            return Err(Box::new(Error {
+                reason: "No activities".to_string(),
+            }))
+        }
+    };
+    let file_name = format!(
+        "{}_{}_{}.json",
+        begin_date.timestamp.format("%Y-%m-%d").to_string(),
+        end_date.timestamp.format("%Y-%m-%d").to_string(),
+        &args.source
+    );
+
+    let handle = match OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(file_name)
+    {
+        Ok(handle) => handle,
+        Err(error) => return Err(Box::new(error)),
+    };
+
+    return match serde_json::to_writer_pretty(handle, &activities) {
+        Ok(_) => Ok(()),
+        Err(error) => Err(Box::new(error)),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use clap::{Args, Parser, Subcommand, ValueEnum};
-use derive_more::Display;
-use serde_json;
-use std::fs::OpenOptions;
-use std::path::Path;
+use clap::{Parser, Subcommand};
 
 mod activity;
+mod convert;
 mod currency;
 mod interactive_brokers;
 mod mbank;
@@ -18,54 +15,18 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
-    Convert(ConvertArgs),
-}
-
-#[derive(Args)]
-struct ConvertArgs {
-    source: ConvertSource,
-    path: String,
-}
-
-#[derive(Display, Clone, ValueEnum)]
-enum ConvertSource {
-    Mbank,
-    InteractiveBrokers,
+    Convert(convert::CommandArgs),
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Command::Convert(args) => {
-            let path = Path::new(&args.path);
-            let mut activities = match &args.source {
-                ConvertSource::Mbank => mbank::convert(&path).unwrap(),
-                ConvertSource::InteractiveBrokers => interactive_brokers::convert(&path).unwrap(),
-            };
+    let result = match &cli.command {
+        Command::Convert(args) => convert::command(&args),
+    };
 
-            activities.sort_by_key(|x| x.timestamp);
-
-            let begin_date = activities
-                .first()
-                .unwrap()
-                .timestamp
-                .format("%Y-%m-%d")
-                .to_string();
-            let end_date = activities
-                .last()
-                .unwrap()
-                .timestamp
-                .format("%Y-%m-%d")
-                .to_string();
-            let file_name = format!("{}_{}_{}.json", begin_date, end_date, &args.source);
-            let mut handle = OpenOptions::new()
-                .write(true)
-                .create(true)
-                .truncate(true)
-                .open(file_name)
-                .unwrap();
-            serde_json::to_writer_pretty(handle, &activities).unwrap();
-        }
+    match result {
+        Err(error) => eprintln!("{}", error),
+        _ => { /* nop */},
     }
 }
