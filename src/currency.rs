299d201e32fffe7421ebf378e17eaff77d9f89eb
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2023-04-16 22:55:30 +0200

    Remove dead code from currency
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

--- src/currency.rs
+++ src/currency.rs
@@ -1,12 +1,11 @@
-use chrono::NaiveDate;
 use derive_more::{Add, Sub};
 use macros;
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+
 use std::fmt;
 use std::fmt::Debug;
-use std::marker::PhantomData;
+
 use std::ops::{Div, Mul};
 
 pub trait Currency: Debug {
@@ -84,30 +83,6 @@ pub struct Eur(pub Decimal);
 )]
 pub struct Gbp(pub Decimal);
 
-#[derive(Debug)]
-pub struct Rate<T> {
-    rates: HashMap<NaiveDate, Decimal>,
-    phantom_data: PhantomData<T>,
-}
-
-impl<T> Rate<T>
-where
-    T: Currency,
-{
-    pub fn new(rates: HashMap<NaiveDate, Decimal>) -> Rate<T> {
-        Rate {
-            rates: rates,
-            phantom_data: PhantomData,
-        }
-    }
-
-    pub fn convert(&self, amount: &T, date: &NaiveDate) -> Result<Pln, ()> {
-        let rate = self.rates.get(date).ok_or(())?;
-        let result = amount.get_value().checked_mul(*rate).unwrap().round_dp(2);
-        Ok(Pln(result))
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -132,19 +107,5 @@ mod tests {
         assert_eq!(b * dec!(2.5), Usd(dec!(17.5)));
         assert_eq!(b / dec!(2), Usd(dec!(3.5)));
         assert_eq!(b / dec!(2.5), Usd(dec!(2.8)));
-    }
-
-    #[test]
-    fn test_rate() {
-        let date1 = NaiveDate::from_ymd_opt(2022, 8, 1).unwrap();
-        let date2 = NaiveDate::from_ymd_opt(2022, 8, 2).unwrap();
-        let date3 = NaiveDate::from_ymd_opt(2022, 8, 3).unwrap();
-        let rates = HashMap::from([(date1, dec!(4.3)), (date2, dec!(4.7))]);
-
-        let rates: Rate<Usd> = Rate::new(rates);
-        let a = Usd(dec!(20.));
-        assert_eq!(rates.convert(&a, &date1), Ok(Pln(dec!(86.))));
-        assert_eq!(rates.convert(&a, &date2), Ok(Pln(dec!(94.))));
-        assert_eq!(rates.convert(&a, &date3), Err(()));
     }
 }
